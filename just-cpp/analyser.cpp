ae7533822bba4523f2586a1e6939b217b75aa261
keeYuc <52500690@qq.com>
2022-02-14 10:20:03 +0800

    add

--- just-cpp/analyser.cpp
+++ just-cpp/analyser.cpp
@@ -4,26 +4,31 @@
 
 typedef enum :int { NORMAL, TOP, LOW }FxType;
 
+typedef struct {
+	int start;
+	int now;
+	int end;
+}indexRange;
 
 class KLine
 {
-    float close;
-    float specialLow;
-    float specialHigh;
-    bool isSpecialHigh();
-    bool isSpecialLow();
+	float close;
+	float specialLow;
+	float specialHigh;
+	bool isSpecialHigh();
+	bool isSpecialLow();
 public:
-    FxType checkFx(KLine& last, KLine& next);
-    bool vaildFx(KLine* last);
+	FxType checkFx(KLine&, KLine&);
+	bool vaildFx(KLine*);
 };
 
 FxType KLine::checkFx(KLine& last, KLine& next)
 {
-    int type = NORMAL;
-    if (this->specialHigh > last.specialHigh && this->specialHigh > next.specialHigh) { type = TOP; };
-    if (this->specialLow < last.specialLow && this->specialLow < next.specialLow) { type += LOW; };
-    if (type >= 3) { type = fabs(this->specialLow - this->close) > fabs(this->specialHigh - this->close) ? LOW : TOP; }
-    return FxType(type);
+	int type = NORMAL;
+	if (this->specialHigh > last.specialHigh && this->specialHigh > next.specialHigh) { type = TOP; };
+	if (this->specialLow < last.specialLow && this->specialLow < next.specialLow) { type += LOW; };
+	if (type >= 3) { type = fabs(this->specialLow - this->close) > fabs(this->specialHigh - this->close) ? LOW : TOP; }
+	return FxType(type);
 }
 
 bool KLine::vaildFx(KLine* last)
