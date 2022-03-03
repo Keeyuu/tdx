98871b8cf46b8c40cf4675edf6205f01aeab26ce
keeYuc <52500690@qq.com>
2022-03-03 17:52:06 +0800

    add todo

--- just-cpp/src/TCalcFuncSets.cpp
+++ just-cpp/src/TCalcFuncSets.cpp
@@ -2,46 +2,48 @@
 #include "TCalcFuncSets.h"
 
 // a: diff ma5 b: open c: close
-void OverfulfilPatter(int DataLen, float* pfOUT, float* pfINa, float* pfINb, float* pfINc)
+void OverfulfilPatter(int dataLen, float* pfOUT, float* pfINa, float* pfINb, float* pfINc)
 {
-	if (DataLen < 1)return;
+	if (dataLen < 1)return;
 	int days = 15;
 	float flag = -9;
-	float sumDiffMa = 0.0; float firstMa = 0.0; float maxLimit = 0.01 * 10;
-	for (int i = 0; i < DataLen; i++) sumDiffMa += pfINa[i];
+	float sumDiffMa = 0.0, firstMa = 0.0, maxLimit = 0.01 * 10;
+	for (int i = 0; i < dataLen; i++) sumDiffMa += pfINa[i];
 	if (sumDiffMa / firstMa >= maxLimit) flag = 1;//最大均线差异幅度
-	if (true);//todo 正突破 子类型 跳空 扎空棒
+	if (isSpecialHigh(dataLen, pfINc, dataLen, days)) {
+		//todo 跳空
+		//todo 扎空棒
+	}
 	if (true);//todo 强轧空
 	if (true);//todo 一星
 	if (true);//todo 二星
 	if (true);//todo 三星
-	for (int i = 0; i < DataLen; i++) pfOUT[i] = flag;
-}
-
-void SingleStarPatter(int DataLen, float* pfOUT, float* pfINa, float* pfINb, float* pfINc)
-{
-}
-
-void DoubleStarsPatter(int DataLen, float* pfOUT, float* pfINa, float* pfINb, float* pfINc)
-{
+	for (int i = 0; i < dataLen; i++) pfOUT[i] = flag;
 }
 
 
-void ThreeStarsPatter(int DataLen, float* pfOUT, float* pfINa, float* pfINb, float* pfINc)
+bool isSpecialHigh(int dataLen, float* close, int cursor, int specialInterval)
 {
+	int left = 0;
+	if (cursor - specialInterval > 0)left = cursor - specialInterval;
+	for (int i = left; i < cursor && i < dataLen; i++) {
+		if (close[i] > close[cursor]) return false;
+	}
+	//todo 寻找底分型 
+	//todo 找到周期内第一根最高点
+	return true;
 }
 
 
-void FindFxPatter(int DataLen, float* pfOUT, float* pfINa, float* pfINb, float* pfINc)
+void FindFxPatter(int dataLen, float* pfOUT, float* pfINa, float* pfINb, float* pfINc)
 {
+
 }
 
 
 PluginTCalcFuncInfo g_CalcFuncSets[] =
 {
-	{1, (pPluginFUNC)&SingleStarPatter},
-	//{2, (pPluginFUNC)&DoubleStarsPatter},
-	//{3, (pPluginFUNC)&ThreeStarsPatter},
+	{1, (pPluginFUNC)&OverfulfilPatter},
 	//{5, (pPluginFUNC)&FindFxPatter},
 	{0, NULL},
 };
